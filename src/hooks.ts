6831453ce7d102824feaf1ae4d002ae0a8e11b9a
roaming97 <roaming98a@gmail.com>
2022-07-11 23:17:36 -0500

    Disable SSR temporarily

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,7 +2,7 @@ import type { Handle } from '@sveltejs/kit'
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const response = await resolve(event, {
-		ssr: true // disable SSR here for SPA
+		ssr: false
 	})
 	return response
 }
